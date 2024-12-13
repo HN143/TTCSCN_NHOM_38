c198431f589468f672878832d57faccc81ce8904
Thanchinh <thannhanchinh2472003@gmail.com>
2024-12-13 12:49:34 +0700

    update 13/12

--- WEBPDF/webpdf/database/serializers.py
+++ WEBPDF/webpdf/database/serializers.py
@@ -15,10 +15,8 @@ class DataSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Bao gồm tất cả các trường của model
 
 class DieuKienTaiSerializer(serializers.ModelSerializer):
-    ngay_bat_dau = serializers.IntegerField(read_only=True)  # Hiển thị timestamp
-    ngay_ket_thuc = serializers.IntegerField(read_only=True)  # Hiển thị timestamp
-    ngay_bat_dau_display = serializers.SerializerMethodField(read_only=True)
-    ngay_ket_thuc_display = serializers.SerializerMethodField(read_only=True)
+    ngay_tai = serializers.IntegerField(read_only=True)  # Hiển thị timestamp
+    ngay_tai_display = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = DieuKienTai
@@ -26,32 +24,20 @@ class DieuKienTaiSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Xử lý timestamp ở đây
-        validated_data['ngay_bat_dau'] = int(
-            datetime.strptime(validated_data.pop('ngay_bat_dau'), "%d/%m/%Y").timestamp() * 1000
-        )
-        validated_data['ngay_ket_thuc'] = int(
-            datetime.strptime(validated_data.pop('ngay_ket_thuc'), "%d/%m/%Y").timestamp() * 1000
+        validated_data['ngay_tai'] = int(
+            datetime.strptime(validated_data.pop('ngay_tai'), "%d/%m/%Y").timestamp() * 1000
         )
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if 'ngay_bat_dau' in validated_data:
-            validated_data['ngay_bat_dau'] = int(
-                datetime.strptime(validated_data.pop('ngay_bat_dau'), "%d/%m/%Y").timestamp() * 1000
-            )
-        if 'ngay_ket_thuc' in validated_data:
-            validated_data['ngay_ket_thuc'] = int(
-                datetime.strptime(validated_data.pop('ngay_ket_thuc'), "%d/%m/%Y").timestamp() * 1000
+        if 'ngay_tai' in validated_data:
+            validated_data['ngay_tai'] = int(
+                datetime.strptime(validated_data.pop('ngay_tai'), "%d/%m/%Y").timestamp() * 1000
             )
         return super().update(instance, validated_data)
 
-    def get_ngay_bat_dau_display(self, obj):
+    def ngay_tai_display(self, obj):
         # Chuyển đổi từ milliseconds sang ngày
-        if obj.ngay_bat_dau:
-            return datetime.fromtimestamp(obj.ngay_bat_dau / 1000).strftime("%d/%m/%Y")
-        return None
-
-    def get_ngay_ket_thuc_display(self, obj):
-        if obj.ngay_ket_thuc:
-            return datetime.fromtimestamp(obj.ngay_ket_thuc / 1000).strftime("%d/%m/%Y")
+        if obj.ngay_tai:
+            return datetime.fromtimestamp(obj.ngay_tai / 1000).strftime("%d/%m/%Y")
         return None
