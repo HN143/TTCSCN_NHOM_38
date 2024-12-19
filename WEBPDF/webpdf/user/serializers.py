551bb6fb2465544873327caef62347d691a021b7
nghiaugust <115493863+nghiaugust@users.noreply.github.com>
2024-12-19 23:45:53 +0700

    băm pass update

--- WEBPDF/webpdf/user/serializers.py
+++ WEBPDF/webpdf/user/serializers.py
@@ -11,3 +11,16 @@ class UserSerializer(ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        # Nếu có trường password trong validated_data, băm password
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+            validated_data.pop('password')  # Xóa password khỏi validated_data để tránh overwrite trực tiếp
+        
+        # Cập nhật các trường khác
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        instance.save()
+        return instance
